d9677bc9554e4f3ccdba6102eeb0d9dfe9785d19
Jason Kubo <kuboj86@gmail.com>
2017-10-19 14:13:05 -0400

    upload

---
+++ Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = numberOne ("Please enter a number between 1-10.");
+
+            if (number > 0 && number < 10)
+                Console.WriteLine("Valid");
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        private static int numberOne(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            int number;
+            if(Int32.TryParse(input, out number))
+            {
+                return number;
+            }
+            else
+            {
+                return numberOne("please enter a valid number");
+            }
+        }
+    }
+}
